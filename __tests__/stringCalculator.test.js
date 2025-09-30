8dbbaec66fa4128111affb9df674c28ed8ec48b0
sudeeshprx <sudeesh.annem@procurextech.com>
2025-09-30 20:29:52 +0530

    test: support newline delimiters

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -18,3 +18,7 @@ test('two numbers comma separated', () => {
 test('multiple numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
 });
+
+test('handles newlines as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
