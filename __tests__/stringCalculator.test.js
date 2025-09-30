e7244113074ececf72b7a351d8c24ba57a578265
sudeeshprx <sudeesh.annem@procurextech.com>
2025-09-30 20:22:47 +0530

    test: add single number test

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -1,6 +1,11 @@
-const {add} = require('../stringCalculator')
+const { add } = require('../stringCalculator')
 
 
 test('empty string returns 0', () => {
     expect(add("")).toBe(0);
-  });
+});
+
+
+test('single number returns its value', () => {
+    expect(add("1")).toBe(1);
+});
