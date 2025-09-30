fc042976a6d37fe40a96da8e80a20eef096ccf92
sudeeshprx <sudeesh.annem@procurextech.com>
2025-09-30 20:31:30 +0530

    feature: handle newline delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
     if (!numbers) return 0;
-    const nums = numbers.split(',').map(n => parseInt(n));
+    const nums = numbers.replace(/\n/g, ',').split(',').map(n => parseInt(n));
     return nums.reduce((a, b) => a + b, 0);
 }
 module.exports = { add };
