d0786b86596d78410c5d99590db301a36c2a90fe
sudeeshprx <sudeesh.annem@procurextech.com>
2025-09-30 20:32:46 +0530

    test: custom delimiter test

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -22,3 +22,7 @@ test('multiple numbers', () => {
 test('handles newlines as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('supports custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
