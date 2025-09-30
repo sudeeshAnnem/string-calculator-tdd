2fae5e71fe048751e0abbf2c29535911a0acac7c
sudeeshprx <sudeesh.annem@procurextech.com>
2025-09-30 20:34:30 +0530

    feature: support custom delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,17 @@
 function add(numbers) {
     if (!numbers) return 0;
-    const nums = numbers.replace(/\n/g, ',').split(',').map(n => parseInt(n));
+
+    let delimiter = /,|\n/;
+
+    let numString = numbers;
+
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        numString = parts[1];
+    }
+
+    const nums = numString.split(delimiter).map(n => parseInt(n));
     return nums.reduce((a, b) => a + b, 0);
 }
 module.exports = { add };
